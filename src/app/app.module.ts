12d5ce5126916be504c74842a4b1387993cbc3f2
Victor <victor@pop-os.localdomain>
2022-06-28 23:18:22 -0500

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ContactmeComponent } from './contactme/contactme.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ContactmeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
