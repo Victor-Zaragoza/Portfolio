4a54b0df1c22d85112d80d99749b68d5e4e07bbc
Victor <victor@pop-os.localdomain>
2022-06-30 23:07:47 -0500

    home components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,9 @@ import { ProjectsComponent } from './projects/projects.component';
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'contactme', component: ContactmeComponent},
-  {path: 'projects', component: ProjectsComponent}
+  {path: 'projects', component: ProjectsComponent},
+  {path: '', component:HomeComponent},
+  {path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
